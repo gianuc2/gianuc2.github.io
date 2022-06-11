7fb97b4f26f2248449e7e1e0964a0d8e72438a57
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2022-06-11 19:36:56 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,31 +1,41 @@
    
 function frames(variable){
     document.getElementById("enframe").src="pages/home.html";
+    document.getElementById("enframe").height="600";
     if (variable==1){
         document.getElementById("enframe").src="pages/home.html";
+        document.getElementById("enframe").height="600";       
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/investigadores.html";
+        document.getElementById("enframe").height="800";       
     }
     else if (variable==3){
         document.getElementById("enframe").src="pages/tesistas-actuales.html";
+        document.getElementById("enframe").height="600";       
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/graduados.html";
+        document.getElementById("enframe").height="600";       
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/proyectos.html";
+        document.getElementById("enframe").height="600";       
     }
     else if (variable==6){
-        document.getElementById("enframe").src="pages/publicaciones.html";  
+        document.getElementById("enframe").src="pages/publicaciones.html"; 
+        document.getElementById("enframe").height="600";       
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/preprints.html";
+        document.getElementById("enframe").height="600";       
     }  
     else if (variable==8){
         document.getElementById("enframe").src="pages/seminario-don-hubert.html";
+        document.getElementById("enframe").height="700";       
     }  
     else if (variable==9){
         document.getElementById("enframe").src="pages/contacto.html";
+        document.getElementById("enframe").height="600";       
     }  
 } 
