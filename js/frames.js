f689e94f2b4b5587c027cc992e655b3f50e67b9f
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2022-06-12 00:43:48 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -36,6 +36,6 @@ function frames(variable){
     }  
     else if (variable==9){
         document.getElementById("enframe").src="pages/contacto.html";
-        document.getElementById("enframe").height="600";       
+        document.getElementById("enframe").height="500";       
     }  
 } 
