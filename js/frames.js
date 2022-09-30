634c85c93a3c70a872d0ba4136b9ecfe40cdbbca
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2022-09-30 16:04:47 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -15,26 +15,22 @@ function frames(variable){
         document.getElementById("enframe").height="1000";       
     }
     else if (variable==4){
-        document.getElementById("enframe").src="pages/graduados.html";
-        document.getElementById("enframe").height="700";       
-    }
-    else if (variable==5){
         document.getElementById("enframe").src="pages/proyectos.html";
         document.getElementById("enframe").height="800";       
     }
-    else if (variable==6){
+    else if (variable==5){
         document.getElementById("enframe").src="pages/publicaciones.html"; 
         document.getElementById("enframe").height="1400";       
     }
-    else if (variable==7){
+    else if (variable==6){
         document.getElementById("enframe").src="pages/preprints.html";
         document.getElementById("enframe").height="600";       
     }  
-    else if (variable==8){
+    else if (variable==7){
         document.getElementById("enframe").src="pages/seminario-don-hubert.html";
         document.getElementById("enframe").height="700";       
     }  
-    else if (variable==9){
+    else if (variable==8){
         document.getElementById("enframe").src="pages/contacto.html";
         document.getElementById("enframe").height="500";       
     }  
