56fb0e05c44e129dbdd3c042a8682903904f9d1c
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2023-03-21 16:29:22 -0300

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -1,18 +1,18 @@
    
 function frames(variable){
     document.getElementById("enframe").src="pages/home.html";
-    document.getElementById("enframe").height="500";
+    document.getElementById("enframe").height="550";
     if (variable==1){
-        document.getElementById("enframe").src="pages/home.html";
-        document.getElementById("enframe").height="500";       
+        document.getElementById("enframe").src="pages/grupo.html";
+        document.getElementById("enframe").height="700";       
     }
     else if (variable==2){
         document.getElementById("enframe").src="pages/investigadores.html";
-        document.getElementById("enframe").height="800";       
+        document.getElementById("enframe").height="730";       
     }
     else if (variable==3){
         document.getElementById("enframe").src="pages/tesistas.html";
-        document.getElementById("enframe").height="1200";       
+        document.getElementById("enframe").height="920";       
     }
     else if (variable==4){
         document.getElementById("enframe").src="pages/proyectos.html";
@@ -20,18 +20,22 @@ function frames(variable){
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/publicaciones.html"; 
-        document.getElementById("enframe").height="1700";       
+        document.getElementById("enframe").height="1900";       
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/preprints.html";
-        document.getElementById("enframe").height="600";       
+        document.getElementById("enframe").height="550";       
     }  
     else if (variable==7){
-        document.getElementById("enframe").src="pages/seminario-don-hubert.html";
-        document.getElementById("enframe").height="1250";       
+        document.getElementById("enframe").src="pages/graduados.html";
+        document.getElementById("enframe").height="650";       
+    }
+    else if (variable==9){
+        document.getElementById("enframe").src="pages/eventos.html";
+        document.getElementById("enframe").height="550";       
     }  
-    else if (variable==8){
+    else if (variable==10){
         document.getElementById("enframe").src="pages/contacto.html";
-        document.getElementById("enframe").height="500";       
+        document.getElementById("enframe").height="550";       
     }  
 } 
