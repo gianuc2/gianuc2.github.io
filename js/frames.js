1cf6d660f84867161944292991c5a937f641073f
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2023-06-16 07:38:53 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -20,7 +20,7 @@ function frames(variable){
     }
     else if (variable==5){
         document.getElementById("enframe").src="pages/publicaciones.html"; 
-        document.getElementById("enframe").height="2100";       
+        document.getElementById("enframe").height="2200";       
     }
     else if (variable==6){
         document.getElementById("enframe").src="pages/preprints.html";
