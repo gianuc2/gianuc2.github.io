e8a08c5a93a51a3d703302a3ecab8fcca52f46b6
gianuc2 <76188759+gianuc2@users.noreply.github.com>
2024-08-02 21:24:00 -0400

    Update frames.js

--- js/frames.js
+++ js/frames.js
@@ -28,7 +28,7 @@ function frames(variable){
     }
     else if (variable==7){
         document.getElementById("enframe").src="pages/preprints.html";
-        document.getElementById("enframe").height="900";       
+        document.getElementById("enframe").height="950";       
     }  
     else if (variable==8){
         document.getElementById("enframe").src="pages/graduados-postgrado.html";
